1475c75330686f60a00306b7a92bdd77e4b46fb4
Patrick Kang <jykang1@gmail.com>
2014-06-01 19:09:56 -0400

    Add show.html

--- js/prettygist.js
+++ js/prettygist.js
@@ -1,8 +1,15 @@
-$(document).ready(function(){
-  try{
-    indexPage();
-  } catch (err){
+var gist_id;
 
+(function(){
+  gist_id = window.location.search.substring(1).slice(0, -1);
+})();
+
+$(document).ready(function(){
+  console.log(gist_id);
+  if(gist_id === ''){
+    indexPage();
+  } else {
+    getGist(gist_id);
   }
 });
 
@@ -13,6 +20,36 @@ var indexPage = function(){
     success : function(data){
       var template = data;
       $('#prettygist').html(data);
+    }
+  });
+};
+
+var getGist = function(gist_id){
+  var url = 'https://api.github.com/gists/' + gist_id;
+  console.log(url);
+  $.ajax({
+    url: url,
+    async: false,
+    dataType: 'json',
+    success: function(data){
+      renderGist(data);
+    }
+  });
+}
+
+var renderGist = function(data){
+  $.ajax({
+    url: 'views/show.html',
+    dataType: 'html',
+    success: function(response){
+      var view, template, html;
+      $('#prettygist').html('');
+      view={
+
+      };
+      template = response;
+      html = Mustache.to_html(template, view);
+      $('#prettygist').append($(html));
     }
   });
 };
