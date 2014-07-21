42f30d39755fe6e1fe3b6780f0ea4866227feb18
Patrick Kang <patrick.kang@tulip.io>
2014-07-21 17:00:24 -0400

    Some style changes

--- js/prettygist.js
+++ js/prettygist.js
@@ -46,6 +46,7 @@ var renderGist = function(data){
 
       view={
         title: data.description,
+        gist_id: data.id,
         owner: data.owner.login,
         gist_url: data.files[filename].raw_url,
         owner_url: data.owner.html_url,
